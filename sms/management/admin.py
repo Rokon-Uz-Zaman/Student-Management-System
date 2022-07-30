260f551a923a7c27648041fe5a1c1ba9566609c3
A.B.M. Rokon-Uz-Zaman Roman <mdroman266@gmail.com>
2022-07-30 17:26:37 +0600

    Update admin.py

--- sms/management/admin.py
+++ sms/management/admin.py
@@ -1,11 +1,39 @@
 from django.contrib import admin 
 from management.models import Student,Teacher,Staff,ClassName
 
+
+admin.site.site_header = 'Student MS'
+admin.site.site_title = 'Student MS'
+
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ('Name','Class_Name','Roll','Active_status')
+	list_filter = ('Name','Class_Name','Roll','Active_status')
+	list_editable = ('Active_status',)
+
+
+
+class TeacherAdmin(admin.ModelAdmin):
+	list_display = ('Name','Position','Active_status')
+	list_filter = ('Name','Position','Active_status')
+	list_editable = ('Active_status',)
+
+
+class ClassNameAdmin(admin.ModelAdmin):
+	list_display = ('Class_Name','Session','Class_Teacher')
+	list_filter = ('Class_Name','Session','Class_Teacher')
+
+class StaffAdmin(admin.ModelAdmin):
+	list_display = ('Name','Position','Active_status')
+	list_filter = ('Name','Position','Active_status')
+	list_editable = ('Active_status',)
+
+
+
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(ClassName)
-admin.site.register(Staff)
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Teacher,TeacherAdmin)
+admin.site.register(ClassName,ClassNameAdmin)
+admin.site.register(Staff,StaffAdmin)
 #admin.site.register(Attendence)
 
 
